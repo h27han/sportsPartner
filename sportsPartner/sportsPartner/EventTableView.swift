bc20edce789c19b1f34029c51448c6e3b0f6bf5e
Laetitia Huang <laetitia3huang@gmail.com>
2016-01-23 22:04:19 -0500

    change segments names

---
+++ sportsPartner/sportsPartner/EventTableView.swift
@@ -0,0 +1,21 @@
+//
+//  EventTableView.swift
+//  sportsPartner
+//
+//  Created by Laetitia Huang on 2016-01-23.
+//  Copyright © 2016 waterlooHacks. All rights reserved.
+//
+
+import UIKit
+
+class EventTableView: UITableView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
